5fffe686be9d6b0464d0cbe3765df789ccdb5fb8
Piyush Gupta <piyushguptaa0001@gmail.com>
2026-08-25 21:01:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (36.95%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> newmat(n,vector<int>(n));
+        int m = matrix[0].size();
+
+        vector<vector<int>> newmat(n, vector<int>(m, 0));
 
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                newmat[j][n-1-i] = matrix[i][j];
+            for(int j=0; j<m; j++){
+               newmat[j][n-1-i] = matrix[i][j];
             }
         }
         matrix = newmat;
